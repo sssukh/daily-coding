5697ec8d77c6269a07b8aafe4cd306cadf8c2898
sssukh <sssukh50@gmail.com>
2021-05-31 00:08:00 +0900

    fail code

--- baek1005-1/source.cpp
+++ baek1005-1/source.cpp
@@ -8,31 +8,76 @@ public:
 	int key;
 	int time;
 	int total_time;
-	vector<Node> Child;
-	vector<Node> Parrent;
+	vector<Node*> Child;
+	vector<Node*> Parrent;
 	Node() {
 		key = NULL;
 		time = NULL;
 		total_time = NULL;
 		
 	}
-	Node(int key){
+	Node(int key, int time){
 		this->key = key;
-		time = 0;
-		total_time = 0;
+		this->time = time;
+		total_time = time;
 		
 		
 	}
-	void insertChild(Node child) {
+	void insertChild(Node* child) {//child에 추가
 		this->Child.push_back(child);
 	}
-	void insertParrent(Node parrent) {
+	void insertParrent(Node* parrent) {//parrent 에 추가
 		this->Parrent.push_back(parrent);
 	}
-
+	void addTime() {
+		for (int i = 0; i < Child.size(); i++) {
+			if (Child[i]->total_time < total_time + Child[i]->time) {
+				Child[i]->total_time = total_time + Child[i]->time;
+			}
+		}
+	}
 	
 };
 
+class Tree {
+public:
+	Node* root;
+	vector<Node*> node_list;
+	Tree() {
+		root = NULL;
+	}
+	Tree(int data,int time) {
+		root = new Node(data,time);
+		node_list.push_back(root);
+	}
+	~Tree(){}
+	void insertNode(int data,int time) {
+		node_list.push_back(new Node(data,time));
+	}
+	void setRoot(Node* node) {
+		root = node;
+	}
+	Node* findNode(int data) {
+		for (int i = 0; i < node_list.size(); i++) {
+			if (node_list[i]->key == data) {
+				return node_list[i];
+			}
+		}
+	}
+	Node* findNodeTime(int data,Node* node) {
+		Node* current = node;
+		for (int i = 0; i < current->Child.size(); i++) {
+
+		}
+	}
+
+	void preOrder(Node* node) {
+		node->addTime();
+		for (int i = 0; i < node->Child.size(); i++) {
+			preOrder(node->Child[i]);
+		}
+	}
+};
 
 int main() {
 	int T;
@@ -40,39 +85,36 @@ int main() {
 	while (T--) {
 		int N, K, W;
 		cin >> N >> K;
-		vector<Node> tree;
-		tree.push_back(Node());//index를 맞추기위해 넣는 empty Node
-		for (int i = 1; i <= N; i++) {//Node 생성 후 time 입력받아 넣기
-			tree.push_back(Node(i));
+		Tree tree;
+		for (int i = 1; i <= N; i++) {//tree초기화
 			int time;
 			cin >> time;
-			tree[i].time = time;
-			tree[i].total_time = time;
+			tree.insertNode(i,time);
+			
 		}
-		while (K--) {
+		
+		
+		while (K--) {//자식관계 입력
 			int a, b;
 			cin >> a >> b;
-			tree[a].insertChild(tree[b]);
-			tree[b].insertParrent(tree[a]);
+			tree.findNode(a)->insertChild(tree.findNode(b));
+			tree.findNode(b)->insertParrent(tree.findNode(a));
 		}
-		/*
-		for (int i = 1; i < tree.size(); i++) {
-			for (int j = 0; j < tree[i].Child.size(); j++) {
-				int tmp;
-				if (tree[i].Child[j].total_time < tree[i].total_time + tree[i].Child[j].time) {
-					tmp = tree[i].total_time + tree[i].Child[j].time;
-					tree[i].Child[j].total_time = tmp;
-					cout << 1 << endl;
-				}
+		for (Node* n : tree.node_list) {//root 설정
+			if (n->Parrent.empty()) {
+				tree.setRoot(n);
+				break;
 			}
 		}
-		*/
+		//cout << "root : " << tree.root->key << endl;
+		//for (Node* n : tree.node_list) {
+		//	n->addTime();
+		//}
+		tree.preOrder(tree.root);//total_time 구하기
 
 		
-		for (Node n : tree) {
-			cout << n.key << " !!! " << n.total_time << endl;
-		}
+		
 		cin >> W;
-		cout << tree[W].total_time<<endl;
+		cout << tree.findNode(W)->total_time<<endl;
 	}
 }
