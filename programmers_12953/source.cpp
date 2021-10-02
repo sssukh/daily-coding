c31dba0ae4a1175da8e059eff44bd0f1d798f125
sssukh <sssukh50@gmail.com>
2021-10-03 02:14:07 +0900

    programmers 12953

--- programmers_12953/source.cpp
+++ programmers_12953/source.cpp
@@ -1,10 +1,37 @@
-#include <string>
 #include <vector>
+#include <unordered_map>
+#include <cmath>
 
 using namespace std;
 
 int solution(vector<int> arr) {
-    int answer = 0;
+    int answer = 1;
+    unordered_map<int, int> map;
+    for (int v : arr)
+    {
+        unordered_map<int, int> counter;
+        int tmp = v;
+        int divisor = 2;
+        while (divisor <= tmp)
+        {
+            if (tmp % divisor == 0)
+            {
+                counter[divisor]++;
+                tmp = tmp / divisor;
+            }
+            else
+            {
+                divisor++;
+            }
+        }
+        for (auto iter : counter)
+        {
+            if (map[iter.first] < iter.second)
+                map[iter.first] = iter.second;
 
+        }
+    }
+    for (auto iter : map)
+        answer *= pow(iter.first, iter.second);
     return answer;
 }
