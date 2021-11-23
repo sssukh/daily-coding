8c5d317a7131963658dbce8a99d38471c62f33b3
sssukh <sssukh50@gmail.com>
2021-11-23 21:00:22 +0900

    programmers_43105

--- programmers_43105/source.cpp
+++ programmers_43105/source.cpp
@@ -6,23 +6,14 @@ using namespace std;
 int solution(vector<vector<int>> triangle) {
     int answer = 0;
 
-    for (int i = 1; i < triangle.size(); i++)
+    for (int i = triangle.size()-2; i >=0 ; i--)
     {
         for (int j = 0; j < triangle[i].size(); j++)
         {
-            if (j == 0)
-                triangle[i][j] += triangle[i - 1][j];
-            else if (j == i)
-                triangle[i][j] += triangle[i - 1][j - 1];
-            else
-                triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j]);
+            triangle[i][j] += max(triangle[i + 1][j], triangle[i + 1][j + 1]);
         }
     }
-    for (int number : triangle.back())
-    {
-        if (number > answer)
-            answer = number;
-    }
+    
 
-    return answer;
+    return triangle[0][0];
 }
