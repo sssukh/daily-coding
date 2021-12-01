6cd3b05f178e97084cf090c60f31f5feb31a3495
sssukh <sssukh50@gmail.com>
2021-12-01 21:49:07 +0900

    problem solving week 13 A

--- problemW13-A/source.cpp
+++ problemW13-A/source.cpp
@@ -3,84 +3,94 @@
 #include <unordered_map>
 using namespace std;
 
-vector<string> map;
-vector<string> words;
-vector<bool> exists;
-
-void check(vector<int>& idx_list,int x, int y)
+vector<int> getFail(string str)
 {
-	vector<pair<int, int>> dir = { {0,1},{1,0},{1,1} };
-	
-	for (int i = 0; i < 3; i++)							//모든 방향 검색
+	vector<int> result(str.size(),0);
+	for (int i = 1,j = 0; i < str.size(); i++)
 	{
-		int addx = dir[i].first;
-		int addy = dir[i].second;
-		int tmp_x = x;
-		int tmp_y = y;
-		for (int j = 0; j < idx_list.size(); j++)		//해당하는 char로 시작하는 모든 단어와 비교
+		while (j > 0 && str[i] != str[j])
 		{
-			string tmp = words[idx_list[j]];
-			
-			int k;
-			for (k = 0; k < tmp.size(); k++)		//단어 비교
-			{
-				if (tmp_x >= map.size() || tmp_y >= map.size() || tmp[k] != map[tmp_x][tmp_y])
-				{
-					tmp_x = x;
-					tmp_y = y;
-					break;
-				}
-				tmp_x += addx; tmp_y += addy;
-			}
-			if (k == tmp.size())
-			{
-				exists[idx_list[j]] = 1;
-				idx_list.erase(idx_list.begin() + j);
-				j--;
-				break;
-			}
+			j = result[j - 1];
 		}
-		
+		if (str[i] == str[j])result[i] = ++j;
 	}
+	return result;
 }
+
+
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+
+
+
 	int N, M;
 	cin >> N >> M;
-	//vector<string> map(N,"");
-	unordered_map<char, vector<int>> hash;
-	//vector<string> words;
-	//vector<bool> exists(M, 0);
+	vector<string> board(N, "");
+	vector<string> words(M, "");
+	string read = "";
+	
 	for (int i = 0; i < N; i++)
 	{
 		string input;
 		cin >> input;
-		//map[i] = input;
-		map.push_back(input);
-	}
-	for (int i = 0; i < M; i++)
-	{
-		string input;
-		cin >> input;
-		words.push_back(input);
-		hash[input[0]].push_back(i);
-		exists.push_back(0);
+		board[i] = input;
+		read += input + "$";
 	}
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < N; j++)
 		{
-			if (!hash[map[i][j]].empty())			//시작하는 단어가 존재하면
+			read += board[j][i];
+		}
+		read += "$";
+	}
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			read += board[N - 1 - i + j][j];
+		}
+		read += "$";
+	}
+	for (int i = 1; i < N; i++)
+	{
+		for (int j = 0; i + j < N; j++)
+		{
+			read += board[j][i + j];
+		}
+		read += "$";
+	}
+	for (int i = 0; i < M; i++) 
+	{
+		string input;
+		cin >> input;
+		vector<int> fail = getFail(input);
+		bool answer = 0;
+		for (int j = 0, k = 0; j < read.size(); j++)
+		{
+			while (k > 0 && input[k] != read[j])
 			{
-				//cout << i << " " << j <<" "<<map[i][j]<< endl;
-				check(hash[map[i][j]], i, j);
+				k = fail[k - 1];
+			}
+			if (input[k] == read[j])
+			{
+				if (k == input.size() - 1)
+				{
+					answer = 1;
+					break;
+				}
+				else
+					k++;
 			}
 		}
+		cout << answer << endl;
 	}
-	for (int i = 0; i < M; i++)
-		cout << exists[i] << endl;
+
+	
+	
+
 }
