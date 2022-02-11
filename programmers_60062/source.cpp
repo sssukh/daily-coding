dc181da2af7774c5ef34b3e61020d91f0df4c75f
sssukh <sssukh50@gmail.com>
2022-02-11 23:48:54 +0900

    programmers 60062

--- programmers_60062/source.cpp
+++ programmers_60062/source.cpp
@@ -10,45 +10,103 @@ bool cmp(int a, int b)
     return a > b;
 }
 
-int solution(int n, vector<int> weak, vector<int> dist) 
-{
+// DFS(bool isClockwise, int start, int idx, int size, vector<int>& weak, vector<int> dist) 
+
+int solution(int n, vector<int> weak, vector<int> dist) //현재 풀이는 weak point들의 범위로 조사하는데 범위아닌 point를 
+{                                                       //커버하는 방향으로 풀어보자
     int answer = -1;
     int weakSize=weak.size();
-    vector<int> between_weak;
-    between_weak.push_back((weak.front() - weak.back() + n)%n);
-    for (int i = 1; i < weakSize; i++)
+    int distSize = dist.size();
+    //sort(dist.begin(), dist.end(),cmp);
+    vector<int> cases;
+    for (int i = 0; i < distSize; i++)
     {
-        between_weak.push_back(weak[i] - weak[i - 1]);
+        cases.push_back(i);
     }
-    sort(dist.begin(), dist.end(),cmp);
-    if (weakSize == dist.size())
-        answer = weakSize;
-    for (int start_adder = 0; start_adder < weakSize; start_adder++)
+
+    do
     {
-        int dist_idx = 0;
-        int tmp = 0;
-        bool hasJumped = false;
-        int between_weak_idx;
-        for (between_weak_idx = 0; between_weak_idx < weakSize; between_weak_idx++)
+        vector<int> tmp_dist;
+        for (int i = 0; i < distSize; i++)
         {
-            tmp += between_weak[(between_weak_idx + start_adder) % weakSize];
-            if (tmp > dist[dist_idx])
+            tmp_dist.push_back(dist[cases[i]]);
+        }
+        for (int start_adder = 0; start_adder < weakSize; start_adder++)
+        {
+            int dist_idx = 0;
+            int clockwise_start = weak[start_adder];
+            int counterclockwise_start = weak[start_adder];
+            bool hasJumped = false;
+            int weak_idx;
+
+            //cout << "start : " << clockwise_start << endl;
+
+            // clockwise
+            for (weak_idx = 1; weak_idx < weakSize; weak_idx++)
             {
-                if (hasJumped)
-                    break;
-                ++dist_idx;
-                tmp = 0;
-                hasJumped = true;
+                int clockwise_tmp = (-clockwise_start + weak[(weak_idx + start_adder) % weakSize] + n) % n;
+
+                cout << "start : " << clockwise_start <<" tmp : "<<clockwise_tmp<< endl;
+
+                if (clockwise_tmp > tmp_dist[dist_idx])
+                {
+
+                    //cout << "next dist" << endl;
+
+                    ++dist_idx;
+                    clockwise_start = weak[(weak_idx + start_adder) % weakSize];
+                }
             }
-            else
-                hasJumped = false;
+
+            if (weak_idx == weakSize)
+            {
+                if (answer == -1 || answer > dist_idx)
+                {
+                    answer = dist_idx + 1;
+                    for (int i = 0; i <= dist_idx; i++)
+                    {
+                        cout << tmp_dist[i] << " ";
+                    }
+                    cout << "dist_idx : "<< dist_idx<<endl;
+                }
+            }
+
+
+            dist_idx = 0;
+
+            // counterclockwise
+            for (weak_idx = 1; weak_idx < weakSize; weak_idx++)
+            {
+                int counterclockwise_tmp = (counterclockwise_start - weak[(weakSize - weak_idx + start_adder) % weakSize] + n) % n;
+                
+                cout << "start : " << counterclockwise_start << " tmp: "<<counterclockwise_tmp<<endl;
+
+                if (counterclockwise_tmp > tmp_dist[dist_idx])
+                {
+
+                    //cout << "next dist" << endl;
+
+                    ++dist_idx;
+                    counterclockwise_start = weak[(weakSize - weak_idx + start_adder) % weakSize];
+                }
+            }
+
+            if (weak_idx == -1)
+            {
+                if (answer == -1 || answer > dist_idx)
+                {
+                    answer = dist_idx + 1;
+                    for (int i = 0; i <= dist_idx; i++)
+                    {
+                        cout << tmp_dist[i] << " ";
+                    }
+                    cout <<"dist_idx : "<<dist_idx<< endl;
+                }
+            }
         }
-        if (between_weak_idx == weakSize)
-        {
-            if (answer == -1 || answer > dist_idx)
-                answer = dist_idx;
-        }
-    }
+    } while (next_permutation(cases.begin(), cases.end()));
+    if (answer > dist.size())
+        answer = -1;
 
     return answer;
 }
